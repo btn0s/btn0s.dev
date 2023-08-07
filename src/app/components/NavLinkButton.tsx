b4ba9da5e0b7b0696f6b92abc1fe3a6c299065c9
Brendan Norris <brendan.t.norris@gmail.com>
2023-08-07 13:50:19 -0400

    redid everything

---
+++ src/app/components/NavLinkButton.tsx
@@ -0,0 +1,37 @@
+"use client";
+
+import useHoverHighlight from "@/hooks/useHoverHighlight";
+import Link from "next/link";
+import { ReactNode, useRef } from "react";
+
+export interface INavLink {
+  icon?: ReactNode;
+  label: string;
+  href: string;
+  isExternal?: boolean;
+}
+
+const NavLinkButton = ({ icon, label, href, isExternal }: INavLink) => {
+  const ref = useRef<HTMLLIElement>(null);
+
+  useHoverHighlight(ref);
+
+  return (
+    <Link
+      href={href}
+      key={href}
+      target={isExternal ? "_blank" : undefined}
+      rel={isExternal ? "noopener noreferrer" : undefined}
+    >
+      <li
+        ref={ref}
+        className="inline-flex h-[42px] w-[42px] items-center justify-center gap-1 rounded-md bg-gray-300 font-bold hover:bg-gray-300 sm:w-auto sm:px-6"
+      >
+        {icon}
+        <span className="sr-only sm:not-sr-only">{label}</span>
+      </li>
+    </Link>
+  );
+};
+
+export default NavLinkButton;
---
+++ src/hooks/useHoverTilt.tsx
@@ -0,0 +1,68 @@
+import { useEffect, RefObject } from "react";
+
+const useHoverTilt = (
+  ref: RefObject<HTMLElement>,
+  {
+    tiltModifier = 1.2,
+    scaleModifier = 1.02,
+  }: {
+    tiltModifier?: number;
+    scaleModifier?: number;
+  } = {},
+) => {
+  useEffect(() => {
+    const handleTilt = (e: MouseEvent | TouchEvent) => {
+      if (ref.current) {
+        const rect = ref.current.getBoundingClientRect();
+        const centerX = rect.left + rect.width / 2;
+        const centerY = rect.top + rect.height / 2;
+
+        let x: number, y: number;
+
+        if (e instanceof MouseEvent) {
+          x = e.clientX;
+          y = e.clientY;
+        } else if (e.touches && e.touches.length > 0) {
+          x = e.touches[0].clientX;
+          y = e.touches[0].clientY;
+        } else {
+          return;
+        }
+
+        const deltaX = (x - centerX) / (rect.width / 2);
+        const deltaY = (y - centerY) / (rect.height / 2);
+
+        const rotationY = tiltModifier * deltaX;
+        const rotationX = -tiltModifier * deltaY;
+
+        ref.current.style.transform = `perspective(500px) rotateX(${rotationX}deg) rotateY(${rotationY}deg) scale(${scaleModifier})`;
+        ref.current.style.transformStyle = "preserve-3d";
+        ref.current.style.transition = "transform 0.1s ease-out";
+        ref.current.style.willChange = "transform";
+      }
+    };
+
+    const handleMouseLeave = () => {
+      if (ref.current) {
+        ref.current.style.transform = "";
+      }
+    };
+
+    const node = ref.current;
+    if (node) {
+      node.addEventListener("mousemove", handleTilt);
+      node.addEventListener("touchmove", handleTilt);
+      node.addEventListener("mouseleave", handleMouseLeave);
+      node.addEventListener("touchend", handleMouseLeave);
+
+      return () => {
+        node.removeEventListener("mousemove", handleTilt);
+        node.removeEventListener("touchmove", handleTilt);
+        node.removeEventListener("mouseleave", handleMouseLeave);
+        node.removeEventListener("touchend", handleMouseLeave);
+      };
+    }
+  }, [ref]);
+};
+
+export default useHoverTilt;
