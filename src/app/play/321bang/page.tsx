906e78c14a64c12b375a24f048af2633ede76301
Brendan Norris <brendan.t.norris@gmail.com>
2023-08-09 22:08:31 -0400

    fixed invite meta

--- src/app/play/321bang/page.tsx
+++ src/app/play/321bang/page.tsx
@@ -1,8 +1,9 @@
 import Game from '@/app/play/321bang/Game';
 import GameRouter from '@/components/GameRouter';
 import GameInfoPage, { IGameInfoPageProps } from '@/templates/GameInfoPage';
-import { Metadata } from 'next';
+import { Metadata, ResolvingMetadata } from 'next';
 import bangCoverImage from '@/images/321bang-cover.png';
+import Head from 'next/head';
 
 const DATA: IGameInfoPageProps = {
   title: '3..2..1.. Bang',
@@ -12,7 +13,7 @@ const DATA: IGameInfoPageProps = {
   playLink: '/play/321bang?p=true',
 };
 
-export const metadata: Metadata = {
+const metadata: Metadata = {
   title: `${DATA.title} | Games by bnt0s`,
   description: DATA.description,
   openGraph: {
@@ -30,6 +31,28 @@ export const metadata: Metadata = {
     card: 'summary_large_image',
   },
 };
+
+type Props = {
+  params: { sid: string };
+  searchParams: { [key: string]: string | string[] | undefined };
+};
+
+export async function generateMetadata(
+  { params, searchParams }: Props,
+  parent: ResolvingMetadata,
+): Promise<Metadata> {
+  const sid = searchParams.sid;
+  const parentResolved = { ...metadata };
+
+  const previousTitle = parentResolved.title;
+
+  return {
+    title: sid ? 'Join my game! | 3..2..1.. BANG! by bnt0s' : previousTitle,
+    description: sid
+      ? 'Come test your reaction time in a 1v1 shootout.'
+      : DATA.description,
+  };
+}
 
 const Bang = () => {
   return (
