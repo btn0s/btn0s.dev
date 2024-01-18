7d2508bd88b2fc03b7ab3c83b6ebb36b724da8cc
Brendan Norris <brendan.t.norris@gmail.com>
2024-01-18 00:23:42 -0700

    added nav to bottom

--- src/app/experiments/layout.tsx
+++ src/app/experiments/layout.tsx
@@ -1,11 +1,25 @@
 import { ReactNode } from "react";
 
 import { Metadata } from "next";
+import Link from "next/link";
+
+import { Button } from "@/components/ui/button";
 
 export const metadata: Metadata = {
   title: "an experiment by @btn0s",
 };
 
 export default function Layout({ children }: { children: ReactNode }) {
-  return <main className="prose prose-invert">{children}</main>;
+  return (
+    <main>
+      <div className="prose prose-invert">{children}</div>
+      <Link href="/" className="flex gap-2 pt-24">
+        <span>✦ bt norris</span>
+        {/* eslint-disable-next-line react/jsx-no-comment-textnodes */}
+        <span className="text-muted-foreground">
+          // designer, programmer, human
+        </span>
+      </Link>
+    </main>
+  );
 }
