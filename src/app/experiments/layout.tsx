5a1181bcc2081b382ebe7deb7f164b6a1381be52
Brendan Norris <brendan.t.norris@gmail.com>
2024-01-18 00:15:57 -0700

    added inspiration

--- src/app/experiments/layout.tsx
+++ src/app/experiments/layout.tsx
@@ -1,5 +1,11 @@
 import { ReactNode } from "react";
 
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "an experiment by @btn0s",
+};
+
 export default function Layout({ children }: { children: ReactNode }) {
   return <main className="prose prose-invert">{children}</main>;
 }
