c38a134678c47395d661d70343e85a64614f2206
b <brendan.t.norris@gmail.com>
2024-01-19 20:00:56 -0700

    updated footer padding

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -8,7 +8,7 @@ const Footer: FC = () => {
   return (
     <footer className="mt-auto flex items-center justify-between gap-2">
       <Link href="/">✦ bt norris</Link>
-      <div className="flex items-center gap-4 px-4 text-muted-foreground underline">
+      <div className="gap-4text-muted-foreground flex items-center underline">
         <Link href="/posts">thoughts</Link>
         <Link href="/experiments">experiments</Link>
       </div>
