26fc9336702dad9d4528f3e36c5a39b61bc0cbba
b <brendan.t.norris@gmail.com>
2024-01-20 00:53:51 -0700

    blur exp tweaks

--- src/components/experiments/ProgressiveBlur.tsx
+++ src/components/experiments/ProgressiveBlur.tsx
@@ -8,23 +8,23 @@ import { ScrollArea } from "@/components/ui/scroll-area";
 export const ProgressiveBlur = () => {
   return (
     <div className="not-prose relative isolate aspect-video rounded-md border border-white/10 bg-white/5">
-      <div className="absolute inset-x-0 top-0 z-[9] h-[57px] backdrop-blur [mask-image:linear-gradient(rgb(0,_0,_0)_55%,_rgba(0,0,0,0.8)_70%,_rgba(0,0,0,0)_100%)]" />
-      <div className="absolute inset-x-0 top-0 z-10 flex justify-between px-4 py-4 mix-blend-difference">
+      <div className="absolute inset-x-0 top-0 z-[9] h-[57px] rounded-t-md bg-black/50 backdrop-blur [mask-image:linear-gradient(rgb(0,_0,_0)_55%,_rgba(0,0,0,0.8)_70%,_rgba(0,0,0,0)_100%)]" />
+      <div className="absolute inset-x-0 top-0 z-10 flex justify-between px-4 py-4 text-white">
         <span>✦ btn0s</span>
         <span>progressive blur</span>
       </div>
       <ScrollArea className="h-full w-full">
         <div className="prose p-4 pt-[calc(57px)]">
           <Image
-            className="mb-2 mt-0 rounded-3xl border border-white/50"
+            className="mb-4 mt-0 rounded-3xl border border-white/50"
             src={imgPpl}
             alt="ppl"
           />
-          <p>
-            They&apos;ve shut down the main reactor. We&apos;ll be destroyed for
-            sure. This is madness! We&apos;re doomed! There&apos;ll be no escape
-            for the Princess this time.
-          </p>
+          <Image
+            className="mb-0 rounded-3xl border border-white/50"
+            src={imgPpl}
+            alt="ppl"
+          />
         </div>
       </ScrollArea>
     </div>
