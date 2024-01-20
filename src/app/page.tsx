e484cad6d729d16db506c7f4357a5ac12a2ed923
b <brendan.t.norris@gmail.com>
2024-01-20 00:55:07 -0700

    fixes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 import { getExperiments } from "@/app/api/experiments";
 import { getPosts } from "@/app/api/posts";
-import { ExternalLinkWithPreview } from "@/components/ExternalLinkWithPreview";
+import { ExternalLinkWithPreview } from "@/components/experiments/ExternalLinkWithPreview";
 import { CURRENT_LINKS } from "@/content/current-links";
 
 export default async function Home() {
@@ -25,9 +25,9 @@ export default async function Home() {
               className="h-4 w-4"
             >
               <path
-                fill-rule="evenodd"
+                fillRule="evenodd"
                 d="M9.58 1.077a.75.75 0 0 1 .405.82L9.165 6h4.085a.75.75 0 0 1 .567 1.241l-6.5 7.5a.75.75 0 0 1-1.302-.638L6.835 10H2.75a.75.75 0 0 1-.567-1.241l6.5-7.5a.75.75 0 0 1 .897-.182Z"
-                clip-rule="evenodd"
+                clipRule="evenodd"
               />
             </svg>
             focus
@@ -94,9 +94,9 @@ export default async function Home() {
                 className="h-4 w-4"
               >
                 <path
-                  fill-rule="evenodd"
+                  fillRule="evenodd"
                   d="M11 3.5v2.257c0 .597.237 1.17.659 1.591l2.733 2.733c.39.39.608.918.608 1.469a2.04 2.04 0 0 1-1.702 2.024C11.573 13.854 9.803 14 8 14s-3.573-.146-5.298-.426A2.04 2.04 0 0 1 1 11.55c0-.551.219-1.08.608-1.47l2.733-2.732A2.25 2.25 0 0 0 5 5.758V3.5h-.25a.75.75 0 0 1 0-1.5h6.5a.75.75 0 0 1 0 1.5H11ZM6.5 5.757V3.5h3v2.257a3.75 3.75 0 0 0 1.098 2.652l.158.158a3.36 3.36 0 0 0-.075.034c-.424.2-.916.194-1.335-.016l-1.19-.595a4.943 4.943 0 0 0-2.07-.52A3.75 3.75 0 0 0 6.5 5.757Z"
-                  clip-rule="evenodd"
+                  clipRule="evenodd"
                 />
               </svg>
               experiments
