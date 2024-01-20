f62467346d1328a2402bb13a7acdf3bc16e040b3
Brendan Norris <brendan.t.norris@gmail.com>
2024-01-20 00:03:57 -0700

    updates

--- src/components/ProgressiveBlur.tsx
+++ src/components/ProgressiveBlur.tsx
@@ -7,7 +7,7 @@ import { ScrollArea } from "@/components/ui/scroll-area";
 
 export const ProgressiveBlur = () => {
   return (
-    <div className="relative isolate aspect-video rounded-md border border-white/10 bg-black/10">
+    <div className="not-prose relative isolate aspect-video rounded-md border border-white/10 bg-white/5">
       <div className="absolute inset-x-0 top-0 z-[9] h-[57px] backdrop-blur [mask-image:linear-gradient(rgb(0,_0,_0)_55%,_rgba(0,0,0,0.8)_70%,_rgba(0,0,0,0)_100%)]" />
       <div className="absolute inset-x-0 top-0 z-10 flex justify-between px-4 py-4 mix-blend-difference">
         <span>✦ btn0s</span>
@@ -21,9 +21,9 @@ export const ProgressiveBlur = () => {
             alt="ppl"
           />
           <p>
-            All work and no play makes Jack a dull boy. All work and no play
-            makes Jack a dull boy. All work and no play makes Jack a dull boy.
-            All work and no play makes Jack a dull boy.
+            They've shut down the main reactor. We'll be destroyed for sure.
+            This is madness! We're doomed! There'll be no escape for the
+            Princess this time.
           </p>
         </div>
       </ScrollArea>
