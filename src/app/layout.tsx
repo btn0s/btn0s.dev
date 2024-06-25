86598edcca1110732fd868cb378197d994c9991b
btn0s <brendan.t.norris@gmail.com>
2024-06-25 07:39:10 -0700

    cleanup

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -33,9 +33,9 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="en" className={`${GeistSans.variable} ${GeistMono.variable}`}>
-      <body className="flex min-h-dvh flex-col gap-12 p-6">
+      <body className="flex min-h-dvh flex-col gap-12">
         <Header />
-        <main className="mx-auto flex max-w-lg flex-col justify-center gap-12 py-24">
+        <main className="flex max-w-lg flex-col gap-12 px-6 py-24">
           {children}
         </main>
         <Footer />
