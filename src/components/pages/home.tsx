86674fc9cbbf18b5ea1f4bdfe8af1f51b7d45d83
btn0s <brendan.t.norris@gmail.com>
2024-06-25 19:25:02 -0700

    polish 5

--- src/components/pages/home.tsx
+++ src/components/pages/home.tsx
@@ -73,9 +73,8 @@ const Home: FC<HomeProps> = ({ experiments, notes }) => {
 
       <HomeSection>
         <h1 className="mb-4 text-sm">
-          I&apos;m a multidisciplinary design engineer building strong teams and
-          powering world class products through tooling, prototyping, systems
-          design, and culture.
+          I&apos;m a multidisciplinary design engineer powering world class
+          products through tooling, prototyping, systems design, and culture.
         </h1>
         <div className="text-sm text-muted-foreground">
           based in Phoenix, Arizona <PiCactusFill className="inline" />
