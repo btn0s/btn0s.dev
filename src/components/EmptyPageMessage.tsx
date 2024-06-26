498802934c2002102aeb79f288e677d8980b0ac5
btn0s <brendan.t.norris@gmail.com>
2024-06-26 15:46:26 -0700

    fixes

--- src/components/EmptyPageMessage.tsx
+++ src/components/EmptyPageMessage.tsx
@@ -3,6 +3,7 @@ import { FC, PropsWithChildren } from "react";
 const EmptyPageMessage: FC<PropsWithChildren> = ({ children }) => {
   return (
     <div className="z-10 text-sm text-muted-foreground opacity-50">
+      {/* eslint-disable-next-line react/jsx-no-comment-textnodes */}
       // nothing here yet. check back later.
     </div>
   );
