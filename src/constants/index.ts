785a6fea0bf8c0f36452a35b0668d01109a3cd3f
btn0s <brendan.t.norris@gmail.com>
2024-06-26 23:01:11 -0700

    add placeholder note

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -3,6 +3,7 @@ import { EntryMetadata } from "@/types";
 export const DEFAULT_ENTRY_METADATA: EntryMetadata = {
   title: "ENTRY_TITLE",
   description: "ENTRY_DESCRIPTION",
+  image: "/og-share-new.png",
   published: false,
   tags: [],
   company: "COMPANY_NAME",
