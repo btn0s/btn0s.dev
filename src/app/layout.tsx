a6ff800f3f5fe105165a064f5e78b87282522b52
btn0s <brendan.t.norris@gmail.com>
2024-09-09 16:12:55 -0700

    update shadcn

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -32,7 +32,10 @@ export const metadata: Metadata = {
 
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
-    <html lang="en" className={`${GeistSans.variable} ${GeistMono.variable}`}>
+    <html
+      lang="en"
+      className={`${GeistSans.variable} ${GeistMono.variable} dark`}
+    >
       <body className="my-auto flex min-h-dvh flex-col gap-12">
         <Header />
         <main className="mx-auto flex w-full max-w-screen-md flex-col gap-12 px-6 py-28">
